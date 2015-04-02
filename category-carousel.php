12a64c78ad5fd168c4b2aca153db6745483acd02
gmlion <gm_l@email.it>
2015-04-02 11:26:55 +0200

    Ripristino CSS corretto, inizio supporto i18n

--- category-carousel.php
+++ category-carousel.php
@@ -7,9 +7,17 @@ Author: Gianmarco Leone
 Author URI: http://www.e-verbavolant.it
 Version: 0.2
 Contains code by Max Rolon from www.barrelny.com
+Text Domain: category-carousel
+Domain Path: /languages/
 */
 require_once('class-CategoryCarousel.php');
 
 $GLOBALS['CategoryCarousel'] = new CategoryCarousel();
+
+function epy_load_plugin_textdomain() {
+  load_plugin_textdomain( 'category-carousel', FALSE, basename( dirname( __FILE__ ) ) . '/languages/' );
+}
+ 
+add_action( 'plugins_loaded', 'epy_load_plugin_textdomain' );
 
 ?>
